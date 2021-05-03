ba7d13bbfeab489731e297ac9079cfbe14451217
pallavidapriya <pallavidapriya75@gmail.com>
2021-05-03 20:11:24 +0530

    the featured is done

--- src/components/chatmessage.js
+++
@@ -1,6 +0,0 @@
-import React, { useRef, useState } from "react";
-
-const ChatMessage = () => {
-  return <React.Fragment>This is ChatMessage in componente</React.Fragment>;
-};
-export default ChatMessage;
--- src/components/signout.js
+++
@@ -1,7 +0,0 @@
-import React, { useRef, useState } from "react";
-
-const SignOut = () => {
-  return <React.Fragment>This is sign in componente</React.Fragment>;
-};
-
-export default SignOut;
